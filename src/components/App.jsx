d3a8fd63207df287fad9539b1f4ebcd55842b501
Nasir2003 <43577897+Nasir2003@users.noreply.github.com>
2021-01-18 17:57:02 -0500

    Day 1, 2 goals done I think... maybe Now to figure out render questions...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,15 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
-function App() {
-  return <div className="app">Trivia!</div>;
-}
+let currentQuestion = 0;
+
+function App() { 
+
+
+  
+  return <div className="app">Trivia!</div>;  
+  return <div className="Question">Question goes here</div>; 
+} 
+
 
 export default App;
